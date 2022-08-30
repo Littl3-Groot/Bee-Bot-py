d12c1bfa9d6dde844143305d0f1e83d04d8cf457
Lélian Thorel <lelian.thorel004@gmail.com>
2022-08-30 18:53:07 +0200

    changements

--- cogs/logs.py
+++ cogs/logs.py
@@ -82,7 +82,7 @@ class Plop(commands.Cog):
         elif before.nick != after.nick:
 
             if after.nick is None:
-                after.nick = before.mention
+                after.nick = before.author
 
             embed = discord.Embed(
                 description=f":writing_hand: **le surnom de {before.mention} à été changé.**", timestamp=datetime.datetime.now(datetime.timezone.utc), color=0xFF9F40)

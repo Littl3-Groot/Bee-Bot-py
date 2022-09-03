dd2512b81660292c49b0ca2263c3f05a24cd086f
Lélian Thorel <lelian.thorel004@gmail.com>
2022-09-03 23:11:04 +0200

    changement

--- cogs/commandes_ammusantes.py
+++ cogs/commandes_ammusantes.py
@@ -154,7 +154,8 @@ class Divers(commands.Cog):
         serverId = serveur.id
         serveurOwner = serveur.owner_id
         banner = ctx.guild.banner_url
-        date_creation = serveur.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        date_creation = serveur.created_at.strftime(
+            "%c")
         all_emotes = list(ctx.guild.emojis)
         emojis = ''.join(
             [str(i) for i in all_emotes])

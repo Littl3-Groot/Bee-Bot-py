fde1672157b3768a4452b34919099f7ad4f6be69
Lélian Thorel <lelian.thorel004@gmail.com>
2022-10-26 12:34:01 +0200

    test dico commande commande

--- cogs/info_commandes.py
+++ cogs/info_commandes.py
@@ -422,6 +422,7 @@ class Info(commands.Cog):
                 
         for (nom_embed, values) in commmandes_dispo.items():
             if nomcommande == values:
+                print(nomcommande, values)
                 await ctx.send(embed=nom_embed)
 
         # for i in range(len(commmandes_dispo)):

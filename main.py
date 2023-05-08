ee6515e683bc067b30768e8004c39765f37e4f16
Lelian <you@example.com>
2023-05-08 10:26:26 +0200

    test

--- main.py
+++ main.py
@@ -162,11 +162,13 @@ async def help(ctx):
         except:
             await ctx.send("Erreur !")
 
-# Ajout de tous les cogs (autres fichiers Python, contenant des commandes, logs ...)
-cogs = [logs.Plop, arrive.Arriver, commandes.Divers, admin.Admin, info_commandes.Info, easter_egg.Easter, errors.ErrorCog]
-
-for cog in cogs:
-    bot.add_cog(cog(bot))
+bot.add_cog(logs.Plop(bot))
+bot.add_cog(arrive.Arriver(bot))
+bot.add_cog(commandes.Divers(bot))
+bot.add_cog(admin.Admin(bot))
+bot.add_cog(info_commandes.Info(bot))
+bot.add_cog(easter_egg.Easter(bot))
+bot.add_cog(errors.ErrorCog(bot))
 
 
 bot.run(TOKEN)

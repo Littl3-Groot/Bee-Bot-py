db11cf6c823b1239791cfeeb7051a293df60d64f
Lelian <you@example.com>
2023-05-26 23:44:36 +0200

    Revert "Delete config.py"
    This reverts commit 5feb4bc2396c02604eeb6c506080fb323a313f20.

---
+++ config.py
@@ -0,0 +1,14 @@
+firebase_config = {
+    "type": "service_account",
+    "project_id": "bee-bot-c47de",
+    "private_key_id": "0f7367775c1606742637efe2b015849090e175c1",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCIhuVLGd8gOkt1\npb2z1WlMZc3WiUCodP9zvWwqnpPKqRam8o6qkpVEoB+qAHorrGBj5W9KD8/Jx36U\n8NE/LreGkOKO5+WoSvS4mef/qFJeiLE/uBwIoZTmrsXgoF6BtzjtZr3RrTJXfJ1o\nu7PRXBst2GVdhn/QCwMXYat1XJnLGZAlMJC/QkryKHWoohP6Cz17hWYEs2k52n1J\n1UYVPOmbKzgO92VisNUjrJUVq0wMV5ZhuscqqbuzzioFzqlx4po7S+qLAbV6VydF\nyki8aFcw5+UTZmQaoV1Euh372RCYJKaDu+WXsxUg96yAck9/+M9J4j6epHsAEXn1\nXpD2vnOrAgMBAAECggEAPY7BJQ8ZvVC7hGTCN6sSasl+OWyyVCJIqflvI3pTve1m\nksjMDdnA5uvhywejDj0PSwuAld6yPFMwXlyEpF3ejxSVqoSh1rNRS0jNw8azuP8e\nGFJdhYDBgA+uld0JL6iayOPp03gpuiusV7RGXDAQ6ydmcPh7eaT1dF3r7qNdjqaj\n8FLznn3irBqnqNGwHETosvEUJrWsF7qk/kJLOpYpWpoarwcoPMJ+Yn+8ofM83XKJ\naJ43cmM07KPyyzTXgrtxi0PZB4Yc5w3hv+cmFHKI+QcagjIWGiHKKaciYPIBEFcm\nd4bFLaZgtCpCPcwf1pi98t3k4xKzsbtj1KVNJEGMcQKBgQC8zkGxJxkk4HUnuvjq\nJugouoASnX+4o3cdkYRh6ciQOJ5MC8pX311cyPIy3I6QqIlhnypHQC6kh2H0gp/K\nF+xfCMoYzYJUHAKSWtJoh1qsLpXs7WeRYDQ3HTY3vB+Vf+9SbDSCCNiI//SBFTIo\npT7KmC2BU+XgLutvrJoaWxEukQKBgQC5HZ3msunSa2PaqWK4vctk+ofTmQSmKoxP\nnq2sIaKsS8iJbhsRzVcOOAM81rokzgvFCPvqvmib1WdrMawLTOOdj2AsHShuM6/A\nxQgwbyCwcvusL1RVPI+IGWiZxZWNs1wcq6DNIZpg7as8knHyr4aXgg3e9Dn4PD2n\n+KoKrJ3UewKBgQChJia5ztZOPGqic5q7OGQVR0HDIbz+2j7z9N0aOVFfj4PFApc9\nMz/bzwOyV8AzRjakJRcP438sHQWFjexWsN6mKrlcPCROJ+4l/vDyGP1SUxzSMbuh\nQIywsfUx0xbeImkhX4vdpTE0lcqaVn4OZSRYBVVpujNNip6XHaoWbr4WkQKBgEzh\nQfzX/UgIlbXwdzGmkVxkuBhnColATnmauyIr7fWEm8qH0fI4x5j+JkoUG8bBVB4G\ntyks3yEpJgRc4sjmq/l53EU17t6l1K5IpjgHTxo5BtDh7zJfLnUlqE2Ew7HP9cdq\neCP5erYavu0vHOffghRWTRB1ecXgrvKd0XERc5MVAoGAFRLwb/X5Tvis91JGk7q4\nxvn+WgCx5KBa9sM+3NJ+tV9FGhdxkgxfKpLt5HlCsAmX1T/yVku2yxhmUb6UkdL+\nCfqPw01jjmfSuYURHysVWSVk7ZAcI5Zf+u8bpTXrIrQGN+vI86+/pzT5Bkow5LsV\n8YPRyKpCrJNR94yc7uVDvH0=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-u9pcs@bee-bot-c47de.iam.gserviceaccount.com",
+    "client_id": "115329251695860749578",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-u9pcs%40bee-bot-c47de.iam.gserviceaccount.com"
+}
+
+DatabaseUrl = "https://bee-bot-c47de-default-rtdb.europe-west1.firebasedatabase.app/"
